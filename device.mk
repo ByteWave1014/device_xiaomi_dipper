e35ee290b0c17e778625f62e775074a708225bcc
Chenyang Zhong <zhongcy95@gmail.com>
2020-12-31 02:00:10 -0500

    dipper: switch to aidl Light HAL
    Change-Id: I3775c2348bc9cac8f4290c976c7d09c421278519
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -167,7 +167,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_dipper
+    android.hardware.light-service.xiaomi_dipper
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
